40e93943eb174b4786658adf47fa39f8bcd01fb1
Dmitriy <svsdima1995@mail.ru>
2021-02-09 12:53:19 +0300

    React компоненты

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
-        <h1>Мой список дел</h1>
-        <input placeholder="search" />
+/* Компонент списка дел */
+const TodoList = () => {
+    return (
         <ul>
             <li>Выучить React</li>
             <li>Создать крутое приложение</li>
         </ul>
-    </div>
-);
+    );
+}
 
-ReactDOM.render(el, 
+/* Компонент шапки */
+const AppHeader = () => {
+    return <h1>Мой список дел</h1>
+}
+
+/* Компонент поиска */
+const SearchPanel = () => {
+    return <input placeholder="search" />
+}
+
+/* Компонент, который собирает другие компоненты в единое целое */
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, 
     document.getElementById('root'));

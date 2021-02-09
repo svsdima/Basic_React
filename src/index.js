e9cccc3628d69ab8871211fc19bfe787fc7b719a
Dmitriy <svsdima1995@mail.ru>
2021-02-09 12:42:10 +0300

    React элементы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+    <div>
+        <h1>Мой список дел</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Выучить React</li>
+            <li>Создать крутое приложение</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, 
+    document.getElementById('root'));
